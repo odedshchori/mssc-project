f1591eb54a82865262ee6eda9900c9ba4338fd7f
Oded <odedshchori@gmail.com>
2024-06-21 16:05:15 +0300

    adding beer repository. closes #5

---
+++ src/main/java/com/oded/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,10 @@
+package com.oded.msscbeerservice.repositories;
+
+import com.oded.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+}
