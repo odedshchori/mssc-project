78df5e8855cac8fc893cd6870d57b864189d9382
Oded <odedshchori@gmail.com>
2024-06-28 22:14:39 +0300

    Merge mssc-beer-service as subdirectory

---
+++ mssc-beer-service/src/main/java/com/oded/msscbeerservice/web/controller/MvcExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.oded.msscbeerservice.web.controller;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class MvcExceptionHandler {
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List<String>> validatrionErrorHandler(ConstraintViolationException e) {
+        List<String> errors = new ArrayList<>(e.getConstraintViolations().size());
+        e.getConstraintViolations().forEach(x-> errors.add(x.getPropertyPath() + " : " + x.getMessage()));
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<List<ObjectError>> handleBindException(BindException e) {
+        return new ResponseEntity<>(e.getAllErrors(), HttpStatus.BAD_REQUEST);
+    }
+}
