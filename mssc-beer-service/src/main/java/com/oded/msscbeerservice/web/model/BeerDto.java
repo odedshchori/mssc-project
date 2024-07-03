8be734bd2a74f5d34f5699b356e88b0c45bb543e
Oded <odedshchori@gmail.com>
2024-07-03 15:35:39 +0300

    Adding Jackson config for BeerDto. closes #13

--- mssc-beer-service/src/main/java/com/oded/msscbeerservice/web/model/BeerDto.java
+++ mssc-beer-service/src/main/java/com/oded/msscbeerservice/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package com.oded.msscbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
@@ -26,9 +27,11 @@ public class BeerDto {
     private Integer version;
 
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ")
     private OffsetDateTime createDate;
 
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ")
     private OffsetDateTime lastModifiedDate;
 
     @NotBlank
@@ -41,9 +44,10 @@ public class BeerDto {
     @NotNull
     private Long upc;
 
-
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
+
     private Integer quantityOnHand;
 }
